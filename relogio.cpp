46d6f01c426fe5163772500a469c1f9e5bf8cae9
user <wesleysrw@gmail.com>
2024-07-20 23:21:40 -0600

    Sincronização inicialmente funcional

---
+++ relogio.cpp
@@ -0,0 +1,201 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <cstring>
+#include <cstdlib> // Para getenv
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+using namespace std;
+using namespace chrono;
+
+struct Dados_horario
+{
+    int horas;
+    int minutos;
+    int segundos;
+};
+
+class Relogio
+{
+private:
+    int horas;
+    int minutos;
+    int segundos;
+    int drift;
+    int portaE;
+    int portaR;
+    string ip_local;
+    bool lider;
+    vector<string> listaIP;
+
+public:
+    Relogio(int h = 0, int m = 0, int s = 0, int d = 1, bool l = true, vector<string> lista  = {"127.0.0.1"})
+        :  horas(h), minutos(m), segundos(s), drift(d), lider(l), listaIP(lista){
+            char* ip_ambiente = getenv("IP");
+            char* porta_ambienteR = getenv("PR");
+            char* porta_ambienteE = getenv("PE");
+            if(ip_ambiente != nullptr && porta_ambienteE != nullptr && porta_ambienteR != nullptr){
+                ip_local = ip_ambiente;
+                portaE = atoi(porta_ambienteE);
+                portaR = atoi(porta_ambienteR);
+            }else{
+                cerr << "Variável de ambiente IP não definida. Usando endereço padrão 127.0.0.1.\n";
+                ip_local = "127.0.0.1";
+                portaE = 9985;
+                portaR = 8885;
+            }
+        }
+    
+    int getHoras(){
+        return horas;
+    }
+
+    int getMinutos(){
+        return minutos;
+    }
+
+    int getSegundos(){
+        return segundos;
+    }
+
+
+    void setDrift(int valor){
+        drift = valor;
+    }
+
+    void setHorario(int h, int m, int s){
+        horas = h;
+        minutos = m;
+        segundos = s;
+    }
+    
+    void engrenagens(){
+        segundos += drift;
+        if(segundos >= 60) {
+            segundos = 0;
+            minutos++;
+            if(minutos >= 60){
+                minutos = 0;
+                horas++;
+            }
+        }
+    }
+
+    void display(){
+        cout << horas << ":" << minutos << ":" << segundos << endl;
+    }
+
+    void enviarHorario() {
+        if(lider == true){
+            int socket_envio;
+            struct sockaddr_in end_servidor;
+            display();
+            engrenagens();
+
+            if((socket_envio = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+                cerr << "Erro ao criar Socket UDP" << "\n";
+                return;
+            }
+
+            //Preenche o bloco de memória apontado com o valor 0 até o número de bytes especificado.
+            memset((char *) &end_servidor, 0, sizeof(end_servidor));
+            end_servidor.sin_family = AF_INET;
+            end_servidor.sin_port = htons(portaE);
+            for(vector<string>::const_iterator iterador = listaIP.begin(); iterador != listaIP.end(); ++iterador){
+                if(inet_aton((*iterador).c_str(), &end_servidor.sin_addr) == 0 ){
+                    cerr << "Endereço não conectado a rede de relogios" << (*iterador) << endl;
+                    close(socket_envio);
+                    return;
+                }
+
+                Dados_horario horario_atual = {horas, minutos, segundos};
+                char buffer[sizeof(Dados_horario)];
+                memcpy(buffer, &horario_atual, sizeof(Dados_horario));
+
+                if(sendto(socket_envio, buffer, sizeof(buffer), 0, (struct sockaddr *) &end_servidor, sizeof(end_servidor)) == -1){
+                    cerr << "Erros ao enviar horario para " << (*iterador) << endl;   
+                } else{
+                    cout << "Relogio enviando horario para " << (*iterador) << ": ";
+                    display();
+                }
+            }
+        }
+
+    }
+
+    void receberHorario() {
+        int socket_receptor;
+        struct sockaddr_in end_servidor, end_cliente;
+        socklen_t end_cliente_size = sizeof(end_cliente);
+        char buffer[sizeof(Dados_horario)];
+
+        if((socket_receptor = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+            cerr << "Erro ao criar Socket UDP" << endl;
+            return;
+        }
+
+        memset((char *) &end_servidor, 0, sizeof(end_servidor));
+        end_servidor.sin_family = AF_INET;
+        //configuração para aceitar conexão com qualquer endereço local
+        end_servidor.sin_addr.s_addr = htonl(INADDR_ANY); //INADDR_ANY tem valor 0
+        end_servidor.sin_port = htons(portaR);
+
+        if(bind(socket_receptor, (struct sockaddr *) &end_servidor, sizeof(end_servidor)) == -1){
+            cerr << "Erro ao se conectar" << endl;
+            close(socket_receptor);
+            return;
+        }
+
+        if(recvfrom(socket_receptor, buffer, sizeof(buffer), 0, (struct sockaddr *) &end_cliente, &end_cliente_size) == -1){
+            cerr << "Erro ao receber horario" << endl;
+        }else{
+            Dados_horario horario_recebido;
+            memcpy(&horario_recebido, buffer, sizeof(Dados_horario));
+
+            if(horario_recebido.horas > horas || (horario_recebido.horas == horas && horario_recebido.minutos > minutos) 
+            || (horario_recebido.horas == horas, horario_recebido.minutos == minutos && horario_recebido.segundos > segundos)){
+                lider = false;
+                horas = horario_recebido.horas;
+                minutos = horario_recebido.minutos;
+                segundos = horario_recebido.segundos;
+                cout << "Relogio atualizado com horario recebido: " << endl;
+                display();
+            }else{
+                lider = true;
+            }
+        }
+
+        close(socket_receptor);
+    }
+
+};
+
+
+int main(){
+    Relogio sincronismo;
+
+    thread enviar_thread([&sincronismo](){
+        while(true){
+            sincronismo.enviarHorario();
+            this_thread::sleep_for(seconds(1));
+        }
+    });
+
+    thread receber_thread([&sincronismo](){
+        while(true){
+            sincronismo.receberHorario();
+        }
+    });
+
+    
+    enviar_thread.join();
+    receber_thread.join();
+
+    return 0;
+}
+
+
+
